2edbcdbeec54de3a90dd896759f0df71baf28520
Nithin Prathapan <nithinprathapan369@gmail.com>
2023-07-24 06:33:26 +0530

    mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
-require('./db/connect')
 const tasks = require("./routes/tasks");
 const app = express();
+const connectDb = require("./db/connect");
+require("dotenv").config();
 
 app.use("/api/v1/tasks", tasks);
 const port = 3000;
-app.listen(port, console.log("Server started on port", port));
+const start = async () => {
+  await connectDb(process.env.MONGO_URL).then(() => {
+    app.listen(port, console.log("Server started on port", port));
+  });
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
-connectionString =
-  "mongodb+srv://nithinvijayprathapan:nithin@cluster0.jy1jxlp.mongodb.net/TASK-MANAGER";
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => console.log("connected to db"))
-  .catch((Err) => {
-    console.log(Err);
-  });
+const connectDb = async (url) => {
+  try {
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("conncected to database...");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDb;
